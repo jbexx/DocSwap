ba975634973048f1ee6ab51eea0da57cbff10f88
Jonathan Beckman <jbecks.media@gmail.com>
2017-09-20 17:13:16 -0600

    add share button and allow user to share via text, email, or other messaging app

--- android/app/src/main/java/com/docswap/MainApplication.java
+++ android/app/src/main/java/com/docswap/MainApplication.java
@@ -3,6 +3,7 @@ package com.docswap;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import cl.json.RNSharePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSharePackage(),
             new VectorIconsPackage(),
             new RNFetchBlobPackage(),
             new PickerPackage(),
